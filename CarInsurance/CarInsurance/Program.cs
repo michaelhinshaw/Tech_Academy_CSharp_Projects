0e7f95e8df3931ccc0850b6bc7380db90d7a79fc
Michael <michaelhinshaw@gmail.com>
2020-10-15 18:50:57 -0700

    Insurance console Application

---
+++ CarInsurance/CarInsurance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI, answer true or false?");
+            bool dUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool isQualified = (age > 15 && dUI == false && tickets < 4);
+            Console.WriteLine("Qualified\n"+isQualified);
+            Console.ReadLine();
+
+        }
+    }
+}
