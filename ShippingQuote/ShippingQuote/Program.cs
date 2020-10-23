aa52ea8ec78329f55d45e344593157abf1294f29
Michael <michaelhinshaw@gmail.com>
2020-10-22 21:14:29 -0700

    a few extra steps here

---
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.ReadLine();
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("The package is too heavy have a nice day.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
