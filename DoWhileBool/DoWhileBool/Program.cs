0b99c4e5fe22f92ad1df29b203f424ee4b7ee606
Michael <michaelhinshaw@gmail.com>
2020-11-11 02:35:46 -0800

    Do while boolean condition

---
+++ DoWhileBool/DoWhileBool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileBool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            do
+            {
+                Console.WriteLine("Counting down... in " + (n));
+                n--;
+            }
+            while (n >= 0);
+            Console.ReadLine();
+        }
+           
+    }
+}
